e462cbc885c4a9f5f88337c6b596b1c8f2564f60
Driadix <driadixa@yandex.ru>
2026-01-19 14:25:27 +0300

    feat: добавить картинки для предзагруженных статей, обновить обработку ошибок для картинок

---
+++ app/migrations/0005_update_images.py
@@ -0,0 +1,36 @@
+﻿# -*- coding: utf-8 -*-
+from django.db import migrations
+
+def update_images(apps, schema_editor):
+    Blog = apps.get_model('app', 'Blog')
+
+    updates = {
+        "Почему мы любим C": "c.jpg",
+        "Vim": "vim.jpg",
+        "8-бит": "retro.jpg",
+        "UNIX": "unix.jpg"
+    }
+
+    for key, filename in updates.items():
+        post = Blog.objects.filter(title__icontains=key).first()
+        if post:
+            post.image = filename
+            post.save()
+
+def reverse_func(apps, schema_editor):
+    Blog = apps.get_model('app', 'Blog')
+    Blog.objects.filter(title__in=[
+        "Возвращение к истокам: Почему мы любим C",
+        "Vim vs Emacs: Вечная война",
+        "Эстетика 8-бит: Когда ограничения рождали шедевры",
+        "Философия UNIX"
+    ]).update(image='temp.jpg')
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0004_blog_image'),
+    ]
+    operations = [
+        migrations.RunPython(update_images, reverse_func),
+    ]
